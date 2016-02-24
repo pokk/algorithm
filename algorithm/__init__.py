a2e8c2b250dd5c9e8c0440981080bbe31d9ba454
Jieyi Wu <wu.jieyi@brilliantservice.co.jp>
2016-02-24 14:04:48 +0900

    1. Change the file name. 2. Add the init file. 3. Add new practicing file.

---
+++ algorithm/__init__.py
@@ -0,0 +1,22 @@
+""" Created by wu.jieyi on 2016/02/24. """
+
+__all__ = ['binary_search', 'combination', 'dfs_bfs', 'merge_sort', 'pascals_formula', 'permutation',
+           'quick_sort', 'tower_of_hanoi', 'tree_insert_find_delete']
+
+"""
+2016/02/08
+The first time practicing program is as below:
+1. binary search.
+2. dfs & bfs search.
+3. merge sort.
+4. quick sort.
+5. tree. (insert & find & delete function)
+
+
+2016/02/04
+The second time practicing program is as below:
+1. combination.
+2. pascal's formula.
+3. permutation.
+4. tower of hanoi.
+"""
