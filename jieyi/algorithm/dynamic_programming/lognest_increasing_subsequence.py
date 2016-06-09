8710ab971d089807c1a6168d7616173a723efd06
Jieyi <pokkbaby@gmail.com>
2016-06-09 18:26:04 +0900

    1. Finish the LIS algorithm.

--- jieyi/algorithm/dynamic_programming/lognest_increasing_subsequence.py
+++ jieyi/algorithm/dynamic_programming/lognest_increasing_subsequence.py
@@ -7,39 +7,41 @@ class LIS:
         self._seq = sequence
         self._length_list_of_seq = [0 for _ in sequence]
 
-    def _lis_len(self):
-        pass
-
     def _lis_algorithm(self):
         for i, v in enumerate(self._seq):
             if i == 0:
-                self._length_list_of_seq[i] = 1
+                self._length_list_of_seq[i] = 1  # Initialize the first one length.
             else:
-                longest_index = self._find_max_length_index(self._length_list_of_seq)
                 less_than = self._less_than(i, self._seq[i])
-                self._length_list_of_seq[i] = self._length_list_of_seq[longest_index] + 1
-                # return self._length_list_of_seq
+                longest_index = self._find_max_length_index(less_than)
+                self._length_list_of_seq[i] = 1 if longest_index == -1 else self._length_list_of_seq[longest_index] + 1
 
     def _find_max_length_index(self, len_list_of_seq):
-        return self._length_list_of_seq.index(max(len_list_of_seq))
+        return -1 if len(len_list_of_seq) == 0 else self._length_list_of_seq.index(max(len_list_of_seq))
 
     def _less_than(self, cur_index, cur_value):
-        less_than_index_list = []
-        for i in range(cur_index):
-            if cur_value > self._seq[i]:
-                less_than_index_list.append(i)
+        return [self._length_list_of_seq[i] for i in range(cur_index) if cur_value > self._seq[i]]
 
-        return less_than_index_list
+    def _backtracking(self):
+        seq = []
+        max_len = max(self._length_list_of_seq)
+        for i in range(len(self._length_list_of_seq) - 1, -1, -1):
+            if max_len == self._length_list_of_seq[i]:
+                seq.append(self._seq[i])
+                max_len -= 1
+
+        return seq
 
     def res(self):
         self._lis_algorithm()
+        seq = self._backtracking()
+        seq.reverse()
 
-        return self._length_list_of_seq
+        return max(self._length_list_of_seq), ' '.join(str(s) for s in seq)
 
 
 def main():
     lis = LIS(lis_str2)
-    print(lis_str2)
     print(lis.res())
 
 
