40a41ba3bb51d492edb5964d0cc41548906247f4
Weian Cheng <weian_cheng@htc.com>
2016-08-29 07:10:03 +0800

    modify the path of Floyd-Warshall algorithm.

--- weian/DynamicProgramming/Floyd_Warshall.py
+++ weian/DynamicProgramming/Floyd_Warshall.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
 
+INF = float("INF")
 
-def Warshall(graph, length):
-    distance = [[float("INF") for _ in range(length)] for _ in range(length)]
+
+def warshall(graph, path_graph, length):
+    distance = [[INF for _ in range(length)] for _ in range(length)]
     path = [[None for _ in range(length)] for _ in range(length)]
 
     for i in range(length):
         for j in range(length):
             distance[i][j] = graph[i][j]
-            if i == j:
-                path[i][j] = None
-            else:
-                path[i][j] = j + 1
-
-    print(path)
+            path[i][j] = path_graph[i][j]
 
     for k in range(length):
         for i in range(length):
             for j in range(length):
-                if i == j:
+                if distance[i][k] == INF or distance[k][j] == INF:
                     continue
                 elif distance[i][j] > distance[i][k] + distance[k][j]:
                     distance[i][j] = distance[i][k] + distance[k][j]
-                    path[i][j] = path[j][k]
+                    path[i][j] = path[k][j]
 
     print(distance)
     print(path)
@@ -39,7 +36,13 @@ def main():
               [5, inf, 3, inf],
               [1, 3, inf, 4],
               [2, inf, 4, inf]]
-    Warshall(graph2, 4)
+
+    path = [[None, 0, 0, 0],
+            [None, None, 1, None],
+            [None, None, None, 2],
+            [3, None, 3, None]]
+
+    warshall(graph, path, 4)
 
 if __name__ == "__main__":
     main()
