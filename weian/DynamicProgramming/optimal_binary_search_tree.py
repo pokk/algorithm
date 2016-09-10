b0ad95e5a3ca86f4013a1d34739d3db60c93fa83
Weian Cheng <weian_cheng@htc.com>
2016-09-10 20:46:47 +0800

    [DP] modify OBST, fix issue.

--- weian/DynamicProgramming/optimal_binary_search_tree.py
+++ weian/DynamicProgramming/optimal_binary_search_tree.py
@@ -13,6 +13,7 @@ def freq_sum(f, i, length):
 def OBST(frequency):
     length = len(frequency)
     cost = [[0 for _ in range(length)] for _ in range(length)]
+    path = [[None for _ in range(length)] for _ in range(length)]
 
     for _ in range(length):
         cost[_][_] = frequency[_]
@@ -21,23 +22,36 @@ def OBST(frequency):
         for i in range(length - l + 1):
             j = i + l - 1
             m = INF
-            for k in range(i, j):
-                print(m)
-                m = min(m, cost[i][k-1] + cost[k+1][j])
-                print(m)
+            small = 0
+            for k in range(i, j+1):
+                if k == i:
+                    left = 0
+                else:
+                    left = cost[i][k-1]
+
+                if k == j:
+                    right = 0
+                else:
+                    right = cost[k+1][j]
+
+                if m > left+right:
+                    small = k
+                m = min(m, left+right)
 
 
-            print("i: " + str(i) + " j: " + str(j) + " m: " + str(m) + " sum: " + str(freq_sum(frequency, i, l)))
             s = m + freq_sum(frequency, i, l)
             cost[i][j] = s
+            path[i][j] = small
 
-    print(cost)
+    return cost, path
 
 
 def main():
     #f = [4, 2, 6, 3]
     f = [34, 8, 50]
-    OBST(f)
+    c, p = OBST(f)
+    print(c)
+    print(p)
 
 if __name__ == "__main__":
     main()
