2bc859532805d52a92f424592de08d627cd49dfa
Weian Cheng <weian_cheng@htc.com>
2016-09-29 17:03:01 +0800

    [DP] create longest arithmetic progression file.

---
+++ weian/DynamicProgramming/longest_arithmetic_progression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+"
+" Author: Weian Cheng
+"
+" Dynamic Programming
+" Longest Arithmetic Progression
+" http://www.geeksforgeeks.org/
+"        length-of-the-longest-arithmatic-progression-in-a-sorted-array/
+"
+"""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
