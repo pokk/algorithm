57b5b954bd3082e2f0205ec688d9352d044947f7
Weian Cheng <weian_cheng@htc.com>
2016-10-01 16:12:21 +0800

    [DP] add the algorithm of longest arithmetic progression.

---
+++ input_data/dynamic_programming/longest_arithmetic_progression.py
@@ -0,0 +1,4 @@
+sequence1 = [1, 7, 10, 15, 27, 29]
+sequence2 = [5, 10, 15, 20, 25, 30]
+
+input_data = [sequence1, sequence2]
--- weian/DynamicProgramming/longest_arithmetic_progression.py
+++ weian/DynamicProgramming/longest_arithmetic_progression.py
@@ -11,9 +11,34 @@
 "
 """
 
+import input_data.dynamic_programming.longest_arithmetic_progression
+
+def longest_arithmetic_progression(sequence):
+    length = len(sequence)
+    result = [2 for _ in range(length)]
+
+    for index in range(length):
+        for i in range(index+1, length):
+            for j in range(i+1 , length):
+                if sequence[i] * 2 - sequence[index] == sequence[j]:
+                    result[j] = result[i] + 1
+
+    maximum = -1
+    for l in result:
+        maximum = max(maximum, l)
+
+    return maximum
+
 
 def main():
-    pass
+    sequences = input_data.dynamic_programming.\
+        longest_arithmetic_progression.input_data
+
+    for sequence in sequences:
+        print("input data: " + str(sequence))
+        print("length of longest arithmetic _progression: " + \
+              str(longest_arithmetic_progression(sequence)))
+        print("\n")
 
 if __name__ == "__main__":
     main()
